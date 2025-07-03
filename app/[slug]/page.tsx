81ef0eae7df1eeea7546ca1d0c0525d615e190b7
Tejas Chavan <64842361+TejasJChavan@users.noreply.github.com>
2025-07-03 18:56:25 +0530

    Last One

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -3,40 +3,46 @@ import { prisma } from "@/lib/db";
 import Link from "next/link";
 import React from "react";
 
-export default async function PagePost(params:any) {
-    const { slug } = params;
+type PageProps = {
+  params: {
+    slug: string;
+  };
+};
 
-    const post = await prisma.post.findUnique({
-        where: {
-            slug: slug,
-        },
-    });
+export default async function PagePost({ params }: PageProps) {
+  const { slug } = params;
 
-    if (!post) return <div>Post Not Found</div>;
+  const post = await prisma.post.findUnique({
+    where: {
+      slug: slug,
+    },
+  });
 
-    return (
-        <div className="flex flex-col h-screen pt-24 place-items-center">
-            <div className="p-10 text-3xl">{post.title}</div>
-            <div className="border-y border-black/10 py-10 w-1/2 flex justify-center">
-                {post.content}
-            </div>
-            <div className="flex p-5 justify-center items-center gap-10">
-                <form action={deletePost}>
-                    <input type="hidden" name="id" value={post.id} />
-                    <button
-                        type="submit"
-                        className="p-3 bg-red-300 rounded-3xl"
-                    >
-                        Delete
-                    </button>
-                </form>
-                <Link
-                    href={`/${post.slug}/update`}
-                    className="p-3 bg-blue-200 rounded-3xl"
-                >
-                    Update
-                </Link>
-            </div>
-        </div>
-    );
+  if (!post) return <div>Post Not Found</div>;
+
+  return (
+    <div className="flex flex-col h-screen pt-24 place-items-center">
+      <div className="p-10 text-3xl">{post.title}</div>
+      <div className="border-y border-black/10 py-10 w-1/2 flex justify-center">
+        {post.content}
+      </div>
+      <div className="flex p-5 justify-center items-center gap-10">
+        <form action={deletePost}>
+          <input type="hidden" name="id" value={post.id} />
+          <button
+            type="submit"
+            className="p-3 bg-red-300 rounded-3xl"
+          >
+            Delete
+          </button>
+        </form>
+        <Link
+          href={`/${post.slug}/update`}
+          className="p-3 bg-blue-200 rounded-3xl"
+        >
+          Update
+        </Link>
+      </div>
+    </div>
+  );
 }
