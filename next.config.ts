ed910a93e51e2494989365c14b7b0191325fb6d0
Tejas Chavan <64842361+TejasJChavan@users.noreply.github.com>
2025-07-03 19:02:11 +0530

    Last

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  devIndicators: false
+    /* config options here */
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    devIndicators: false,
 };
 
 export default nextConfig;
